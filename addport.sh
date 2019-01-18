6d32ad901ccd9fbe07970e74cbd416f315597381
abhinav dixit <abhinav.dixit#truringsoftek.co.in>
2019-01-18 08:39:28 +0000

    new version

--- addport.sh
+++ addport.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #ONE TIME ADDING PORT
 
-#comment the below line to hardcode the port number
-#var1=$1
-var1=enp0s3
-tc qdisc del dev $var1 root
-tc qdisc add dev $var1 root handle 1:0 htb default 1234
-tc class add dev $var1 parent 1:0 classid 1:1234 htb rate 1kbps ceil 1kbps prio 99
+var1=enp3s0f1
+var2=enp3s0f0
+#DOWNLINK PORT
+tc qdisc add dev $var1 root handle 1:0 htb
+#UPLINK PORT
+tc qdisc add dev $var2 root handle 2:0 htb
 
+#tc class add dev $var1 parent 1:0 classid 1:1234 htb rate 1kbps ceil 1kbps prio 99
 #the default class above "1234" redirect default traffic to 1kbps speed
-
 #it gives an error RTNETLINK : No such file or directory
 #this is because we try to del dev $var1 where there is no device intially
